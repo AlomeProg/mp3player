617a95f89ad66f9f7f7ecd1a480bdab76596698d
AlomeProg <alomeprog@gmail.com>
2024-03-02 19:12:49 +0300

    Воссоздана прошлая функциональность + упрощение логики работы + добавление плейлиста

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <string>
 #include <map>
+#include <numeric>
 #include <functional>
 
 #include "raylib.h"
@@ -10,6 +11,10 @@
 // Переменные для элементов интерфейса
 std::map<std::string, Rectangle> gui_rects;
 std::map<std::string, std::function<void(void)>> gui_funcs;
+int lv_playlist_scroll_index = 0;
+int lv_playlist_active = -1;
+int current_active = -1;
+std::string lv_playlist_text = "";
 
 // Глобальные переменные (общие для всего приложения)
 Music music;                          // Текущая музыка
@@ -19,30 +24,43 @@ std::vector<std::string> name_files;  // Имя файла для отображ
 // Функции для интерфейса
 void LoadMusic(void)
 {
+  if(full_paths.empty()) return;
+  if(current_active == lv_playlist_active) return;
+
   TraceLog(LOG_INFO, "LOAD");
   if(IsMusicReady(music))
     UnloadMusicStream(music);
-  music = LoadMusicStream("music.mp3");
+  music = LoadMusicStream(full_paths.at(lv_playlist_active).c_str());
+  current_active = lv_playlist_active;
 }
 
 void BtnPlay(void)
-{}
+{
+  TraceLog(LOG_INFO, "PLAY");
+  PlayMusicStream(music);
+}
 
 void BtnPause(void)
-{}
+{
+  PauseMusicStream(music);
+}
 
 void BtnStop(void)
-{}
+{
+  StopMusicStream(music);
+}
 
 // Функции для работы интерфейса
 void InitGUI(void)
 {
-  gui_rects["btn_play"] = {0.0f, 0.0f, 96.0f, 32.0f};
+  gui_rects["btn_play"] = {10.0f, 350.0f, 100.0f, 40.0f};
   gui_funcs["btn_play"] = BtnPlay;
-  gui_rects["btn_pause"] = {0.0f, 0.0f, 96.0f, 32.0f};
+  gui_rects["btn_pause"] = {120.0f, 350.0f, 100.0f, 40.0f};
   gui_funcs["btn_pause"] = BtnPause;
-  gui_rects["btn_stop"] = {0.0f, 0.0f, 96.0f, 32.0f};
+  gui_rects["btn_stop"] = {230.0f, 350.0f, 100.0f, 40.0f};
   gui_funcs["btn_stop"] = BtnStop;
+  
+  gui_rects["lv_playlist"] = {340.0f, 10.0f, 250.0f, 380.0f};
 }
 
 void UpdateGUI(void)
@@ -50,13 +68,17 @@ void UpdateGUI(void)
   if(GuiButton(gui_rects["btn_play"], "Play")) gui_funcs["btn_play"]();
   if(GuiButton(gui_rects["btn_pause"], "Pause")) gui_funcs["btn_pause"]();
   if(GuiButton(gui_rects["btn_stop"], "Stop")) gui_funcs["btn_stop"]();
+  GuiListView(gui_rects["lv_playlist"], lv_playlist_text.c_str(), &lv_playlist_scroll_index, &lv_playlist_active);
+  if(lv_playlist_active != -1) LoadMusic();
 }
 
 int main()
 {
-  InitWindow(800, 600, "mp3player");
+  InitWindow(600, 400, "mp3player");
   InitAudioDevice();
   InitGUI();
+
+  GuiSetStyle(0, 16, 20);
 
   while (!WindowShouldClose())
   {
@@ -79,7 +101,16 @@ int main()
         TraceLog(LOG_INFO, "Path load: %s", full_paths.at(i).c_str());
       for (int i = 0; i < name_files.size(); i++)
         TraceLog(LOG_INFO, "Name file: %s", name_files.at(i).c_str());
+      lv_playlist_text = std::accumulate
+      (
+        std::next(name_files.begin()), name_files.end(),name_files[0],
+          [](std::string a, std::string b) 
+          {
+            return a + ';' + b;
+          }
+      );
     }
+    UpdateMusicStream(music);
 
     BeginDrawing();
     ClearBackground(BLACK);
